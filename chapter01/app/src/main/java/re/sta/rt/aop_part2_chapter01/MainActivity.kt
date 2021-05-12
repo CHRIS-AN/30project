18f9adea6be2dec8aff3ab3a3860713a441b8ecf
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-13 04:10:58 +0900

    BMI 계산기 (1)

--- chapter01/app/src/main/java/re/sta/rt/aop_part2_chapter01/MainActivity.kt
+++ chapter01/app/src/main/java/re/sta/rt/aop_part2_chapter01/MainActivity.kt
@@ -2,10 +2,18 @@ package re.sta.rt.aop_part2_chapter01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val heightEditText : EditText  아무것도 할당해주지 않았다면? error 발생한다.
+        val heightEditText : EditText = findViewById(R.id.heightEditText) // 명시적
+        val weightEditText = findViewById<EditText>(R.id.weightEditText) // 추론적
+        val resultButton = findViewById<Button>(R.id.resultButton)
+
     }
 }
