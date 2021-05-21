623e8829ac63a9c701817de5ee5876c065cf0597
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-22 08:59:00 +0900

    setOnclick setting

--- chapter03/app/src/main/java/re/sta/rt/aop_part2_chapter03/MainActivity.kt
+++ chapter03/app/src/main/java/re/sta/rt/aop_part2_chapter03/MainActivity.kt
@@ -2,10 +2,59 @@ package re.sta.rt.aop_part2_chapter03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+
+    // lazy 유닛에서 넘버피커를 이니셜라이징 해주는 과정에서 apply 함수를 통해 초기화
+
+    private val numberPicker1 : NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val numberPicker2 : NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    private val numberPicker3 : NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3)
+            .apply {
+                minValue = 0
+                maxValue = 9
+            }
+    }
+
+    // 오픈 버튼
+    private val openButton : AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+    }
+
+    // 패스워드 변경 버튼
+    private val changePasswordButton : AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+        openButton.setOnClickListener {
+             
+        }
+
     }
 }
