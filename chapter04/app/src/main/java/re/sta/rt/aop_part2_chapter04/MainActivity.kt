96768085f87e196625ab8c1def3bb1f99473c230
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-22 16:29:07 +0900

    chpater04 계산기 settings

---
+++ chapter04/app/src/main/java/re/sta/rt/aop_part2_chapter04/MainActivity.kt
@@ -0,0 +1,11 @@
+package re.sta.rt.aop_part2_chapter04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
