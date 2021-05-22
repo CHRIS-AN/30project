5b045f06ca5198694219eb500448c81677df82eb
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-22 15:25:58 +0900

    비밀번호 설정/변경/mapping 여부

--- chapter03/app/src/main/java/re/sta/rt/aop_part2_chapter03/MainActivity.kt
+++ chapter03/app/src/main/java/re/sta/rt/aop_part2_chapter03/MainActivity.kt
@@ -1,9 +1,14 @@
 package re.sta.rt.aop_part2_chapter03
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.NumberPicker
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,6 +48,8 @@ class MainActivity : AppCompatActivity() {
         findViewById<AppCompatButton>(R.id.changePasswordButton)
     }
 
+    // 패스워드 변경 중에, 어떠한 기능도 사용할 수 없게 막기.
+    private var changePasswordMode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,8 +60,74 @@ class MainActivity : AppCompatActivity() {
         numberPicker3
 
         openButton.setOnClickListener {
-             
+
+            if(changePasswordMode) {
+                Toast.makeText(this, "비밀번호 변경 중입니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener // 이 람다식만 하고 나간다고 설정해두는 것.
+                                        // 이렇게 return을 하지 않을경우에, setOnClickListener 를 나가게 된다.
+            }
+
+
+            // numberPicker 1,2,3 랑 비교를하기 때문에, 기기에 입력한 번호를 저장해주어야한다.
+
+            //sharedPreference를 가지고 저장되어있는 값을 가져오기로한다.
+                            // password라는 preference 라는 파일을 다른 앱과 같이 사용할 수 있게,
+                            // share를 하는 것인데, 다른 앱과 공유를하지 않고, 나만 사용할 수 있게 모드를 private을 설정하낟.
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+            if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                // 패스워드 성공
+
+                //TODO 다이어리 페이지 작성 후에 넘겨주어야 합니다.
+                //startActivity()
+            }else {
+                // 패스워드 실패
+                showErrorAlertDialog()
+            }
         }
 
+
+        // 비밀번호를 바꾸는 기능
+        changePasswordButton.setOnClickListener {
+
+            val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+            if(changePasswordMode) {
+                passwordPreferences.edit(true) {
+                    putString("password", passwordFromUser)
+                }
+                // commit 을 통해 저장이 끝났기 때문에,
+                changePasswordMode = false
+                changePasswordButton.setBackgroundColor(Color.BLACK)
+
+            }else {
+                // changePasswordMode 가 활성화 :: 비밀번호가 맞는지를 체크해줘야한다.
+                if (passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+
+                    // 이 곳은 패스워드 변경 모드 !
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 패스워드를 입력해주세요", Toast.LENGTH_SHORT).show()
+
+                    // changePassword 모드가 활성화 되었음을 알려주기 위함.
+                    changePasswordButton.setBackgroundColor(Color.RED)
+                }else {
+                    // 패스워드 실패
+                    showErrorAlertDialog()
+                }
+            }
+        }
+    }
+
+    // 패스워드 실패 함수.
+    private fun showErrorAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Failed")
+            .setMessage("비밀번호가 잘못 되었습니다.")
+            .setPositiveButton("확인") { _, _ -> }
+            .create()
+            .show()
     }
 }
