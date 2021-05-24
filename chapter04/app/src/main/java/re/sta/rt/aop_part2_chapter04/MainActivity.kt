2537dd1fb231a2bd6d3190134554cf3a257b61cf
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-25 07:39:14 +0900

    mainActivity작업1

--- chapter04/app/src/main/java/re/sta/rt/aop_part2_chapter04/MainActivity.kt
+++ chapter04/app/src/main/java/re/sta/rt/aop_part2_chapter04/MainActivity.kt
@@ -13,6 +13,34 @@ class MainActivity : AppCompatActivity() {
 
     // layout에 있는, android : onClick 과 연결
     fun buttonClicked (v : View) {
+        when(v.id) {
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+
+            R.id.buttonPlus -> operatorButtonClicked("+")
+            R.id.buttonMinus -> operatorButtonClicked("-")
+            R.id.buttonMulti -> operatorButtonClicked("*")
+            R.id.buttonDivider -> operatorButtonClicked("/")
+            R.id.buttonModulo -> operatorButtonClicked("%")
+
+        }
+    }
+
+    // 연산자 버튼 클릭 시,
+    private fun operatorButtonClicked(operator : String) {
+
+    }
+
+    // button 들이 눌렸을 때?
+    private fun numberButtonClicked(number: String) {
 
     }
 
