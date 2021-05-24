7fd18b766ba0d9c926986969acb1b350b3841fc5
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-25 06:36:41 +0900

    계산기1

--- chapter04/app/src/main/java/re/sta/rt/aop_part2_chapter04/MainActivity.kt
+++ chapter04/app/src/main/java/re/sta/rt/aop_part2_chapter04/MainActivity.kt
@@ -2,10 +2,17 @@ package re.sta.rt.aop_part2_chapter04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    // layout에 있는, android : onClick 과 연결
+    fun buttonClicked (v : View) {
+
     }
 }
