ca07a2ec92de032bbdb5f6477a6c155da505c014
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-26 04:20:58 +0900

    모델에 @

--- chapter04/app/src/main/java/re/sta/rt/aop_part2_chapter04/model/historyModel.kt
+++ chapter04/app/src/main/java/re/sta/rt/aop_part2_chapter04/model/historyModel.kt
@@ -1,10 +1,12 @@
 package re.sta.rt.aop_part2_chapter04.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class History (
-    val uid : Int?,
-    val expression : String?,
-    val result : String?
+    @PrimaryKey val uid : Int?,
+    @ColumnInfo(name = "expression") val expression : String?,
+    @ColumnInfo(name = "result") val result : String?
 )
