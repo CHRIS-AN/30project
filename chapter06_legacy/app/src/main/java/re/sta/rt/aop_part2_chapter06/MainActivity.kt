92eaa44ffdb910e82c0fb99e103740fed01b215b
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-28 06:03:42 +0900

    updateRemainTime()

--- chapter06_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter06/MainActivity.kt
+++ chapter06_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter06/MainActivity.kt
@@ -3,20 +3,21 @@ package re.sta.rt.aop_part2_chapter06
 import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.SeekBar
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private val remainMinutesTextView : TextView by lazy {
+    private val remainMinutesTextView: TextView by lazy {
         findViewById(R.id.remainMinutesTextView)
     }
 
-    private val remainSecondsTextView : TextView by lazy {
+    private val remainSecondsTextView: TextView by lazy {
         findViewById(R.id.remainSecondsTextView)
     }
 
-    private val seekBar : SeekBar by lazy {
+    private val seekBar: SeekBar by lazy {
         findViewById(R.id.seekBar)
     }
 
@@ -30,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun bindViews() {
         seekBar.setOnSeekBarChangeListener(
-            object : SeekBar.OnSeekBarChangeListener{
+            object : SeekBar.OnSeekBarChangeListener {
                 @SuppressLint("SetTextI18n")
                 override fun onProgressChanged(
                     seekBar: SeekBar?,
@@ -49,6 +50,25 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         )
+    }
+
+    private fun createCountDownTimer(initialMillis: Long) =
+        object : CountDownTimer(initialMillis, 1000L) {
+            // countdown 생성 하자마자 바로 실행.
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+    
+    @SuppressLint("SetTextI18n")
+    private fun updateRemainTime(remainMillis : Long) {
+        val remainSeconds = remainMillis / 1000
+
+        remainMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
     }
 
 
