79fdfd7611ebdf75b9343bce1af03887e972f43c
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-28 04:41:17 +0900

    Seekbar 와 TextView 연결

--- chapter06_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter06/MainActivity.kt
+++ chapter06_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter06/MainActivity.kt
@@ -1,11 +1,55 @@
 package re.sta.rt.aop_part2_chapter06
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView : TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondsTextView : TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+
+    private val seekBar : SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
     }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener{
+                @SuppressLint("SetTextI18n")
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    // 사용자가 했건 안했건 상관하지 않고, progress
+                    remainMinutesTextView.text = "%02d".format(progress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                }
+            }
+        )
+    }
+
+
 }
