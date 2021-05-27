fdc4c9dbf730ddf30d8e1125c36bab53e8fdb455
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-27 21:45:16 +0900

    권한 팝업 Permission

--- chapter05_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter05/MainActivity.kt
+++ chapter05_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter05/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -33,7 +34,8 @@ class MainActivity : AppCompatActivity() {
                     // TODO 권한이 잘 부여되었을 때, 갤러리에서 사진을 선택하는 기능
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
-                    // todo 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                    //  교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                    showPermissionContextPopup()
                 }
                 else -> {
                     requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
@@ -41,6 +43,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun showPermissionContextPopup() {
+        // 알럿다이얼로그,
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("Allow") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }.setNegativeButton("Deny") { _, _ -> }
+            .create()
+            .show()
+
+    }
+
     private fun initStartPhotoFrameModeButton() {
 
     }
