6f0c34ebb17144da222b35e8af8451298d237679
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-29 14:26:10 +0900

    권한을 승인,거절 관련하여 조절.

--- chapter07_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter07/MainActivity.kt
+++ chapter07_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter07/MainActivity.kt
@@ -3,6 +3,8 @@ package re.sta.rt.aop_part2_chapter07
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.Manifest
+import android.content.pm.PackageManager
+
 /*
     (기능)
     Request runtime permissions (사생활보호 권한)
@@ -36,6 +38,28 @@ class MainActivity : AppCompatActivity() {
         requestAudioPermission()
         initViews()
     }
+
+    // 요청한 권한에대해서 결과값을 받는다.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // 권한이 부여가 됐다면, 그냥 그대로있고? 만약 권한이 거절당하면 앱종료!
+
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION &&
+                grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        // 만약 recordPermission 권한이 부여가[ 되지 않았다면?
+        if(!audioRecordPermissionGranted) {
+            finish()
+        }
+    }
+
+
+    // 권한이 가게끔 만들기.
     private fun requestAudioPermission() {
         requestPermissions(requiredPermission, REQUEST_RECORD_AUDIO_PERMISSION)
     }
