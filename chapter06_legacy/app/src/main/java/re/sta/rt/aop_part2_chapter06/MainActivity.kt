7589a66841c416c2b7d28ecd296646d53cc77ae0
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-05-29 11:48:35 +0900

    tickingSounds 설정 및 background 나갈 시, stop

--- chapter06_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter06/MainActivity.kt
+++ chapter06_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter06/MainActivity.kt
@@ -1,12 +1,23 @@
 package re.sta.rt.aop_part2_chapter06
 
 import android.annotation.SuppressLint
+import android.media.SoundPool
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.SeekBar
 import android.widget.TextView
 
+
+/*
+    SoundPool 이란?
+
+        오디오파일을 메모리에 로드를 한 뒤에, 비교적 빠르게 재생을 할 수 있게 도와줌
+        긴 것은 힘들고, 짧은 것만 가능하게끔? 제약이 되어있다 !
+
+
+
+ */
 class MainActivity : AppCompatActivity() {
 
     private val remainMinutesTextView: TextView by lazy {
@@ -20,6 +31,13 @@ class MainActivity : AppCompatActivity() {
     private val seekBar: SeekBar by lazy {
         findViewById(R.id.seekBar)
     }
+    private val soundPool = SoundPool.Builder().build()
+
+
+    private var currentCountDownTimer : CountDownTimer? = null
+
+    private var tickingSoundId : Int? = null
+    private var bellSoundId : Int? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,26 +45,69 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         bindViews()
+        // 로드하는 작업
+        initSounds()
+    }
+
+    // 앱이 보이지 않았다가, 다시 앱으로 돌아올 경우,
+    override fun onResume() {
+        super.onResume()
+
+        soundPool.autoResume()
+    }
+
+
+    // 앱이 화면에서 보이지 않을 경우,
+    override fun onPause() {
+        super.onPause()
+
+        soundPool.autoPause()
+    }
+
+    private fun initSounds() {
+        // 두 개의 파일들을 로드하기 위한 메소드
+        // 이렇게하면 로드된 파일 반환하게 된다 ↓
+       tickingSoundId =  soundPool.load(this, R.raw.timer_ticking, 1) // 값 1을 넣는 건 호환성이 좋다고 하여 그렇게 함.
+       bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
     }
 
     private fun bindViews() {
         seekBar.setOnSeekBarChangeListener(
             object : SeekBar.OnSeekBarChangeListener {
-                @SuppressLint("SetTextI18n")
+               // @SuppressLint("SetTextI18n")
                 override fun onProgressChanged(
                     seekBar: SeekBar?,
                     progress: Int,
                     fromUser: Boolean
                 ) {
                     // 사용자가 했건 안했건 상관하지 않고, progress
-                    remainMinutesTextView.text = "%02d".format(progress)
+                    //remainMinutesTextView.text = "%02d".format(progress)
+
+                   if(fromUser) {
+                    updateRemainTime(progress * 60 * 1000L)
+                   }
+
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    // count다운 도중에, 새로운 카운트를 설정을할 경우에를 막기 위함.
+                    currentCountDownTimer?.cancel() // 현재 카운트를 stop
+                    currentCountDownTimer = null
+
                 }
-
+                // 이 시점에서 바로 countDown 이 시작된다.
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    // seeKBar 가 nullable 할 때 나오는 에외처리.
+                    // 이 경우에는 countdowntimer를 시작하지 못하게 return 시킨다.
+                    seekBar ?: return
 
+                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+                    currentCountDownTimer?.start()
+
+                    // 카운트 다운이 시작함과 동시에,
+                    tickingSoundId?.let { soundId ->
+                    soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+                    }
                 }
             }
         )
@@ -56,10 +117,16 @@ class MainActivity : AppCompatActivity() {
         object : CountDownTimer(initialMillis, 1000L) {
             // countdown 생성 하자마자 바로 실행.
             override fun onTick(millisUntilFinished: Long) {
+
+                // 갱신
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
             }
 
             override fun onFinish() {
-
+                updateRemainTime(0)
+                updateSeekBar(0)
+                
             }
         }
     
@@ -71,5 +138,8 @@ class MainActivity : AppCompatActivity() {
         remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
     }
 
-
+    private fun updateSeekBar(remainMillis: Long) {
+        // seeBar 는 하나하나가 '분'을 나타낸다.
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
+    }
 }
