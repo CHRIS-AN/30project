81cc2c9966beebeb6c37f28b1d48545fbac97d14
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-06-03 04:16:32 +0900

    키보드 Done/ javascript enabled

--- chapter08_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter08/MainActivity.kt
+++ chapter08_legacy/app/src/main/java/re/sta/rt/aop_part2_chapter08/MainActivity.kt
@@ -2,6 +2,11 @@ package re.sta.rt.aop_part2_chapter08
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.ImageButton
 
 /*
     웹브라우저 앱
@@ -18,8 +23,47 @@ import android.os.Bundle
  */
 
 class MainActivity : AppCompatActivity() {
+
+    private val webView : WebView by lazy { findViewById(R.id.webView) }
+    private val addressBar : EditText by lazy { findViewById(R.id.addressBar) }
+    private val goHomeButton : ImageButton by lazy { findViewById(R.id.goHomeButton) }
+    private val goBackButton : ImageButton by lazy { findViewById(R.id.goBackButton) }
+    private val goFowardButton : ImageButton by lazy { findViewById(R.id.goForwardButton) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews();
+        bindViews();
+
+    }
+
+    // 뷰를 초기화하는 init 뷰
+    private fun initViews() {
+
+        webView.apply {
+            webViewClient =  WebViewClient() // 외부웹 브라우저로 가지 않고, 우리가 지정한 웹뷰를 사용함.
+            settings.javaScriptEnabled = true // 자바스크립트를 사용하겠다
+            loadUrl("http://www.google.com")
+        }
+    }
+
+    private fun bindViews() { // 액션이 발생한 뷰(1), 액션아이디 안에는 액션던한 uri?(2) ?(3)
+        addressBar.setOnEditorActionListener { v, actionId1, event ->
+            if(actionId1 == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(v.text.toString())
+            }// done 한 뒤에, 키보드를 내려야하기 때문에 false를 사용
+            return@setOnEditorActionListener false
+        }
+
+        goBackButton.setOnClickListener {
+            webView.goBack() // 이전에 있던 history로 돌아갈 수 있다.
+        }
+        goFowardButton.setOnClickListener {
+            webView.goForward()
+        }
+
     }
 }
