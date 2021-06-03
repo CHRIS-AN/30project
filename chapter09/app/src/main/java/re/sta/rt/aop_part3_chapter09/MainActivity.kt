98116616f5eebd6d41e1d4019f4f7d4ab9b876a4
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-06-04 05:06:17 +0900

    UI 세팅

--- chapter09/app/src/main/java/re/sta/rt/aop_part3_chapter09/MainActivity.kt
+++ chapter09/app/src/main/java/re/sta/rt/aop_part3_chapter09/MainActivity.kt
@@ -2,10 +2,28 @@ package re.sta.rt.aop_part3_chapter09
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
+
+/*
+    Firebase 토큰 확인
+    일반/확장형/커스텀 알림 확인
+
+    기능>
+    Firebase Cloud Messaging
+    Notification
+
+ */
 
 class MainActivity : AppCompatActivity() {
+
+    private val resultTextView : TextView by lazy { findViewById(R.id.resultTextView)}
+    private val firebaseToken : TextView by lazy { findViewById(R.id.firebaseTokenTextView)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
