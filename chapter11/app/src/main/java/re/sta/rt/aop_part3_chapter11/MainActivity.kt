f5165e0df6466861d6c1765bbf3dc08de9ff3a71
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-06-09 07:56:29 +0900

    part3 - chapger11 settings

---
+++ chapter11/app/src/main/java/re/sta/rt/aop_part3_chapter11/MainActivity.kt
@@ -0,0 +1,18 @@
+package re.sta.rt.aop_part3_chapter11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+
+    AlarmManager 
+    Notification
+    Broadcast receiver
+
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
