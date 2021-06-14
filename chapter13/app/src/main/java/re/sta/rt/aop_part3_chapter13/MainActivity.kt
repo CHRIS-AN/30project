1c7bb93ade14f3ff428fd9050ab1d513d5bab3df
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-06-15 03:57:52 +0900

    Firebase 세팅 설정

--- chapter13/app/src/main/java/re/sta/rt/aop_part3_chapter13/MainActivity.kt
+++ chapter13/app/src/main/java/re/sta/rt/aop_part3_chapter13/MainActivity.kt
@@ -8,7 +8,18 @@ import android.os.Bundle
 
     틴더 앱
 
+    Firebase Authentication 사용하기
+        - Email Login
+        - Facebook Login
 
+    Firebase Realtime Database 사용하기
+    yuyakaido / CardStackView 사용하기
+
+
+
+ Firebase Authentication 을 통해 이메일 로그인/ 페이스북 로그인을 할 수 있음.
+ Firebase Realtime Database 를 이용하여 기록을 저장하고, 불러올 수 있음
+ Github 에서 Opensource Library 를 찾아 사용할수 있음.
 
  */
 class MainActivity : AppCompatActivity() {
@@ -17,3 +28,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
