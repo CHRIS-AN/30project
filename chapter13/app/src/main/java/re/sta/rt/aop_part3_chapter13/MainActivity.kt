d9eb935f4795a02c97155ac8beac088ce80a8387
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-06-14 21:16:49 +0900

    Tinder App Settings

---
+++ chapter13/app/src/main/java/re/sta/rt/aop_part3_chapter13/MainActivity.kt
@@ -0,0 +1,19 @@
+package re.sta.rt.aop_part3_chapter13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+/*
+
+    틴더 앱
+
+
+
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
