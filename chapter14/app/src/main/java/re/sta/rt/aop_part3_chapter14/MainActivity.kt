2279bf89a8805747ecdcfa69a789e8d369b692da
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2021-06-18 03:53:15 +0900

    fragment attach

--- chapter14/app/src/main/java/re/sta/rt/aop_part3_chapter14/MainActivity.kt
+++ chapter14/app/src/main/java/re/sta/rt/aop_part3_chapter14/MainActivity.kt
@@ -2,6 +2,11 @@ package re.sta.rt.aop_part3_chapter14
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import re.sta.rt.aop_part3_chapter14.chatList.ChatListFragment
+import re.sta.rt.aop_part3_chapter14.home.HomeFragment
+import re.sta.rt.aop_part3_chapter14.myPage.MyPageFragment
 
 
 /*
@@ -15,8 +20,6 @@ import android.os.Bundle
     Firebase Authentication
  */
 
-
-// TEST
 class MainActivity : AppCompatActivity() {
 
 
@@ -25,6 +28,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        val homeFragment = HomeFragment()
+        val chatListFragment = ChatListFragment()
+        val myPageFragment = MyPageFragment()
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        // 처음에는 아무것도 attach 되어있지 않기 때문에? 초기 설정
+        replaceFragment(homeFragment)
+        
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(homeFragment)
+                    R.id.chatList -> replaceFragment(chatListFragment)
+                R.id.myPage -> replaceFragment(myPageFragment)
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragmentContainer, fragment)  // 우리가 인자로 받아온 fragment 로 뷰를 교체하겠다.
+                commit()
+            }
     }
 }
